57096ee8a701cc49ef795e616c4e25856a054f43
Ju Hyeon Park <wheredidjupark@gmail.com>
2015-05-13 15:17:42 -0700

    complete functional instantiation queue

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -1,18 +1,36 @@
 var Queue = function(){
   var someInstance = {};
+  var size = 0;
+  //
+  var firstItem = 0;
+  var lastItem = 0;
 
   // Use an object with numeric keys to store values
   var storage = {};
 
   // Implement the methods below
 
+  //identical to array except that it cannot unshift & it cannot pop.
   someInstance.enqueue = function(value){
+    storage[lastItem++] = value;
+    size++;
   };
 
   someInstance.dequeue = function(){
+
+    //access the first item
+    //decrease the size by one
+    if(size === 0) return undefined;
+
+    var value = storage[firstItem];
+    firstItem++;
+    size--;
+    return value;
+
   };
 
   someInstance.size = function(){
+    return size;
   };
 
   return someInstance;
