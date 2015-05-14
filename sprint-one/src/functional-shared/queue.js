421dbbb16f88f3591887032f47db19e3ac06579a
Ju Hyeon Park <wheredidjupark@gmail.com>
2015-05-14 13:19:54 -0700

    complete functional-shared stacks and queues

--- sprint-one/src/functional-shared/queue.js
+++ sprint-one/src/functional-shared/queue.js
@@ -1,9 +1,41 @@
 var Queue = function(){
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
+  // but try not not reference your old code in writinge xh
+
+  var obj = {};
+  _.extend(obj, queueMethods, {
+    _begin: 0,
+    _end: 0,
+    _size: 0,
+    _storage: {}
+  });
+
+
+  return obj;
+
 };
 
-var queueMethods = {};
+
+var queueMethods = {
+
+  enqueue: function(value){
+    this._storage[this._end]= value;
+    this._size++;
+    this._end++;
+  },
+  dequeue: function(){
+    if(!(this._size <= 0)){
+      var value = this._storage[this._begin];
+      this._size--;
+      this._begin++;
+      return value;
+    }
+
+  },
+  size: function(){
+    return this._size;
+  }
 
 
+};
 
--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,8 +1,33 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var obj = {};
+  _.extend(obj,{
+    _size: 0,
+    _top: 0,
+    _storage: {}
+  },
+  stackMethods);
+  return obj;
 };
 
-var stackMethods = {};
+var stackMethods = {
 
+  push: function(value){
+    this._size++;
+    this._storage[++this._top] = value;
+  },
+
+  pop: function(){
+    if(this._size === 0) return undefined;
+    var val = this._storage[this._top];
+    this._size--;
+    this._top--;
+    return val;
+  },
+
+  size: function(){
+    return this._size;
+  }
+};
 
