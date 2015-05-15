61738ee34e91d9733700cba99e2ae659030beed1
Ju Hyeon Park <wheredidjupark@gmail.com>
2015-05-15 10:52:28 -0700

    complete linkedlist

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -1,15 +1,45 @@
+
 var LinkedList = function(){
   var list = {};
   list.head = null;
   list.tail = null;
 
   list.addToTail = function(value){
+    var node = Node(value);
+
+    if(!this.tail){
+      this.tail = node;
+      this.head = node;
+    } else {
+      this.tail.next = node; //
+      this.tail = node;
+    }
+
   };
 
   list.removeHead = function(){
+    //var temp = this.head;
+    var newHead = this.head.next;
+    var prevHeadVal = this.head.value;
+    delete this.head; //good or bad for performance?
+    this.head = newHead;
+
+    return prevHeadVal;
   };
 
   list.contains = function(target){
+
+    var pos = this.head;
+
+    while(pos){ // no pos.next!
+      if(pos.value === target){
+        return true;
+      } else {
+        pos = pos.next;
+      }
+    }
+
+    return false;
   };
 
   return list;
