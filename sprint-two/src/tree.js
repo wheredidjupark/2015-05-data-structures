1fd07e6ef3596481d5842bc88461ce4688fbf051
Ju Hyeon Park <wheredidjupark@gmail.com>
2015-05-15 11:23:22 -0700

    complete tree implementation

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -1,9 +1,11 @@
 var Tree = function(value){
   var newTree = {};
-  newTree.value = value;
+  newTree.value = value;//"node"
+
+  _.extend(newTree, treeMethods);
 
   // your code here
-  newTree.children = null;  // fix me
+  newTree.children = [];
 
   return newTree;
 };
@@ -15,10 +17,29 @@ var Tree = function(value){
 var treeMethods = {};
 
 treeMethods.addChild = function(value){
-
+  this.children.push(Tree(value));
 };
 
-treeMethods.contains = function(target){
+treeMethods.contains = function(target, node){
+  node = node || this;
+  var results = [];
+  if(node.value === target){
+    return true;
+  }
+
+  for(var i = 0; i < node.children.length; i++){
+    results.push(this.contains(target, node.children[i]));
+  }
+
+  return _.some(results,function(value){
+    return value === true;
+  });
+
+/*
+  _.reduce(node.children,function(){
+
+  });
+*/
 
 };
 
