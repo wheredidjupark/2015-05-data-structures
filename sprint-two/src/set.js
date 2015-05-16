45f0d702ccad422d86a737c8a72133568957d051
Ju Hyeon Park <wheredidjupark@gmail.com>
2015-05-16 01:05:19 -0700

    set.js

--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -1,18 +1,21 @@
 var Set = function(){
   var set = Object.create(setPrototype);
-  set._storage = undefined;
+  set._storage = {};
   return set;
 };
 
 var setPrototype = {};
 
 setPrototype.add = function(item){
+	this._storage[item] = true;
 };
 
 setPrototype.contains = function(item){
+	return !!this._storage[item];
 };
 
 setPrototype.remove = function(item){
+	delete this._storage[item];
 };
 
 /*
